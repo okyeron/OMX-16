1942cc87de0d5a9297bd2bc473356d5468e2d6ad
okyeron <1909661+okyeron@users.noreply.github.com>
2022-02-23 21:46:20 -0600

    add mozzi test

--- OMX-16-firmware/OMX-16-firmware.ino
+++ OMX-16-firmware/OMX-16-firmware.ino
@@ -24,6 +24,30 @@ char prodstr[32] = "OMX-16";
 #include <Adafruit_DotStar.h>
 Adafruit_DotStar dotstar(1, 41, 40, DOTSTAR_BRG);
 
+# MOZZI BIZ
+#include <MozziGuts.h>
+#include <Oscil.h>
+#include <tables/saw_analogue512_int8.h> // oscillator waveform
+#include <tables/cos2048_int8.h> // filter modulation waveform
+#include <LowPassFilter.h>
+#include <mozzi_rand.h>  // for rand()
+#include <mozzi_midi.h>  // for mtof()
+
+Oscil<SAW_ANALOGUE512_NUM_CELLS, AUDIO_RATE> aOsc1(SAW_ANALOGUE512_DATA);
+Oscil<SAW_ANALOGUE512_NUM_CELLS, AUDIO_RATE> aOsc2(SAW_ANALOGUE512_DATA);
+Oscil<SAW_ANALOGUE512_NUM_CELLS, AUDIO_RATE> aOsc3(SAW_ANALOGUE512_DATA);
+Oscil<SAW_ANALOGUE512_NUM_CELLS, AUDIO_RATE> aOsc4(SAW_ANALOGUE512_DATA);
+Oscil<SAW_ANALOGUE512_NUM_CELLS, AUDIO_RATE> aOsc5(SAW_ANALOGUE512_DATA);
+Oscil<COS2048_NUM_CELLS, CONTROL_RATE> kFilterMod(COS2048_DATA);
+
+LowPassFilter lpf;
+uint8_t resonance = 170; // range 0-255, 255 is most resonant
+uint8_t notes[] = {33, 34, 31}; // possible notes to play MIDI A1, A1#, G1
+uint16_t note_duration = 17500;
+uint8_t note_id = 0;
+uint32_t lastMillis = 0;
+
+
 U8G2_FOR_ADAFRUIT_GFX u8g2_display;
 
 // Declare NeoPixel strip object:
@@ -122,10 +146,17 @@ void setup() {
 	dotstar.show();            // Turn OFF all pixels ASAP
 	dotstar.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
 
+	startMozzi();
+	kFilterMod.setFreq(0.08f);
+	lpf.setCutoffFreqAndResonance(20, resonance);
+	setNotes();
+
 }
 
 void loop() {
 	keypad.tick();
+
+	audioHook();
 
 	while(keypad.available()){
 		auto e = keypad.next();
@@ -145,6 +176,46 @@ void loop() {
 
   delay(10);
 }
+
+
+void setNotes() {
+  float f = mtof(notes[note_id]);
+  aOsc2.setFreq( f + (float)rand(100)/100); // orig 1.001, 1.002, 1.004
+  aOsc3.setFreq( f + (float)rand(100)/100);
+  aOsc4.setFreq( f + (float)rand(100)/100);
+  aOsc5.setFreq( (f/2) + (float)rand(100)/1000);  
+}
+
+// mozzi function, called every CONTROL_RATE
+void updateControl() {
+  // filter range (0-255) corresponds with 0-8191Hz
+  // oscillator & mods run from -128 to 127
+  byte cutoff_freq = 67 + kFilterMod.next()/2;
+  lpf.setCutoffFreqAndResonance(cutoff_freq, resonance);
+  
+  if(rand(CONTROL_RATE) == 0) { // about once every second
+    Serial.println("!");
+    kFilterMod.setFreq((float)rand(255)/4096);  // choose a new modulation frequency
+    setNotes(); // wiggle the tuning a little
+  }
+  
+  if( millis() - lastMillis > note_duration )  {
+    lastMillis = millis();
+    note_id = rand(3); // (note_id+1) % 3;
+    Serial.println((byte)notes[note_id]);
+  }
+}
+// mozzi function, called every AUDIO_RATE to output sample
+AudioOutput_t updateAudio() {
+  long asig = lpf.next(aOsc1.next() + 
+                       aOsc2.next() + 
+                       aOsc3.next() +
+                       aOsc4.next() +
+                       aOsc5.next()
+                       );
+  return MonoOutput::fromAlmostNBit(11,asig);
+}
+
 
 
 void u8g2centerText(const char* s, int16_t x, int16_t y, uint16_t w, uint16_t h) {
